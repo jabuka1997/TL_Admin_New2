29762626bbd9e09322c7dca6dc370b06d44fe1b3
jabuka1997 <Administrator@NB8FK38.htl-donaustadt.at>
2017-03-28 10:52:37 +0200

    First Commit

---
+++ app/src/main/java/com/example/administrator/tl_admin_new/Start_Fenster.java
@@ -0,0 +1,13 @@
+package com.example.administrator.tl_admin_new;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Start_Fenster extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start__fenster);
+    }
+}
